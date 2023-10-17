2fde6710f35ec9455079610b89c261afcdacac57
Esther Faith <efk@mit.edu>
2023-10-17 01:51:54 -0400

    non empty files now

---
+++ client/views/NotificationView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+
+const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
+</script>

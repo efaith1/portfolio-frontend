702b45c06624eb6935a5ccaddd066216906df833
Esther Faith <efk@mit.edu>
2023-10-23 20:21:20 -0400

    ready to synchronize clow

---
+++ client/components/Limit/DecrementLimit.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { fetchy } from "../../utils/fetchy";
+
+const content = ref(0);
+const type = ref("");
+
+const decrementLimit = async (content: number, type: string) => {
+  try {
+    await fetchy("/api/limits/resource", "PUT", {
+      body: { content, type },
+    });
+  } catch (_) {
+    return;
+  }
+};
+</script>
---
+++ client/components/Limit/ResetLimit.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { fetchy } from "../../utils/fetchy";
+
+const type = ref("");
+
+const resetLimit = async (type: string) => {
+  try {
+    await fetchy("/api/limits/reset", "PUT", {
+      body: { type },
+    });
+  } catch (_) {
+    return;
+  }
+};
+</script>

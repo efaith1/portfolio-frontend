832556f65a25746ede5879847bd00a9da44f7032
Esther Faith <efk@mit.edu>
2023-10-27 05:02:51 -0400

    some cool css

--- client/App.vue
+++ client/App.vue
@@ -22,54 +22,70 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <header>
-    <nav>
-      <div class="title">
-        <img src="@/assets/images/logo.svg" />
-        <RouterLink :to="{ name: 'Home' }">
-          <h1>ProductiVerse</h1>
-        </RouterLink>
-      </div>
-      <ul>
-        <li>
-          <RouterLink :to="{ name: 'Home' }" :class="{ underline: currentRouteName == 'Home' }"> Home </RouterLink>
-        </li>
-        <li v-if="isLoggedIn">
-          <RouterLink :to="{ name: 'MyPosts' }" :class="{ underline: currentRouteName == 'MyPosts' }"> Posts </RouterLink>
-        </li>
+  <div class="App-container">
+    <header>
+      <nav>
+        <div class="title">
+          <img src="@/assets/images/gears.jpeg" />
+          <RouterLink :to="{ name: 'Home' }">
+            <h1>ProductiVerse</h1>
+          </RouterLink>
+        </div>
+        <ul>
+          <li>
+            <RouterLink :to="{ name: 'Home' }" :class="{ underline: currentRouteName == 'Home' }"> Home </RouterLink>
+          </li>
+          <li v-if="isLoggedIn">
+            <RouterLink :to="{ name: 'MyPosts' }" :class="{ underline: currentRouteName == 'MyPosts' }"> Posts </RouterLink>
+          </li>
 
-        <li v-if="isLoggedIn">
-          <RouterLink :to="{ name: 'LikedPosts' }" :class="{ underline: currentRouteName == 'LikedPosts' }"> Liked </RouterLink>
-        </li>
-        <li v-if="isLoggedIn">
-          <RouterLink :to="{ name: 'Notifications' }" :class="{ underline: currentRouteName == 'Notifications' }"> Notifications </RouterLink>
-        </li>
-        <li v-if="isLoggedIn">
-          <RouterLink :to="{ name: 'Settings' }" :class="{ underline: currentRouteName == 'Settings' }"> Settings </RouterLink>
-        </li>
-        <li v-if="isLoggedIn">
-          <RouterLink :to="{ name: 'Limit' }" :class="{ underline: currentRouteName == 'Limit' }"> Limit </RouterLink>
-        </li>
-        <li v-else>
-          <RouterLink :to="{ name: 'Login' }" :class="{ underline: currentRouteName == 'Login' }"> Login </RouterLink>
-        </li>
-      </ul>
-    </nav>
-    <article v-if="toast !== null" class="toast" :class="toast.style">
-      <p>{{ toast.message }}</p>
-    </article>
-  </header>
-  <RouterView />
+          <li v-if="isLoggedIn">
+            <RouterLink :to="{ name: 'LikedPosts' }" :class="{ underline: currentRouteName == 'LikedPosts' }"> Liked </RouterLink>
+          </li>
+          <li v-if="isLoggedIn">
+            <RouterLink :to="{ name: 'Notifications' }" :class="{ underline: currentRouteName == 'Notifications' }"> Notifications </RouterLink>
+          </li>
+          <li v-if="isLoggedIn">
+            <RouterLink :to="{ name: 'Settings' }" :class="{ underline: currentRouteName == 'Settings' }"> Settings </RouterLink>
+          </li>
+          <li v-if="isLoggedIn">
+            <RouterLink :to="{ name: 'Limit' }" :class="{ underline: currentRouteName == 'Limit' }"> Limit </RouterLink>
+          </li>
+          <li v-else>
+            <RouterLink :to="{ name: 'Login' }" :class="{ underline: currentRouteName == 'Login' }"> Login </RouterLink>
+          </li>
+        </ul>
+      </nav>
+      <article v-if="toast !== null" class="toast" :class="toast.style">
+        <p>{{ toast.message }}</p>
+      </article>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
 @import "./assets/toast.css";
 
+.App-container {
+  max-width: 960px;
+  padding: var(--m) var(--m);
+  margin: auto;
+  background-image: url(../public/split.jpeg);
+  position: absolute;
+  background-position: center;
+  background-repeat: repeat;
+  background-size: cover;
+  min-width: 100%;
+  min-height: 100%;
+}
+
 nav {
   padding: 1em 2em;
-  background-color: var(--base-pink);
+  background-color: rgb(177, 192, 240);
   display: flex;
   align-items: center;
+  box-shadow: 4px 4px 8px rgba(0.1, 0.1, 0.1, 0.4);
 }
 
 h1 {
